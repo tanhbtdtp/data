11b430b90df3061db26e38ccd5ac125b1f46046e
Thanh tan Home <tanhbt.dtp@gmail.com>
2022-10-24 22:54:08 +0700

    profile edit

--- Pages/Profile.js
+++ Pages/Profile.js
@@ -1,4 +1,4 @@
-import { useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -8,112 +8,134 @@ import {
   View,
   Image,
   FlatList,
+  SafeAreaView,
 } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import App_Color from "../Themes/Color";
 import APIs from "../Services/APIs";
 
-
 export default function Profile() {
-const [listData,setlistData] = useState();
+  const [listData, setlistData] = useState();
 
-useEffect(() => {
-  const url = APIs.getUsersAPI;
-  const fetchData = async () => {
+  useEffect(() => {
+    const url = APIs.getUsersAPI;
+    const fetchData = async () => {
       try {
-          const response = await fetch(url);
-          const json = await response.json();
-          //console.log(json);
-          setlistData(json);
-          //console.log(listData);
+        const response = await fetch(url);
+        const json = await response.json();
+        //console.log(json);
+        setlistData(json);
+        //console.log(listData);
       } catch (error) {
-          console.log("error", error);
+        console.log("error", error);
       }
-  };
-  fetchData();
-
-},[]);
+    };
+    fetchData();
+  }, []);
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <Header />
-      <Content data={listData}/>
+      <Content data={listData} />
     </View>
   );
 }
 
 const Header = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: App_Color.MauNen, width: "100%" }}>
+    <View
+      style={{
+        height: 150,
+        backgroundColor: App_Color.MauNen,
+        width: "100%",
+        alignItems: "center",
+      }}
+    >
       <View
         style={{
           flex: 1,
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
           width: "90%",
+          justifyContent: "center",
+          marginTop: 50,
         }}
       >
-        <Image
-          style={styles.imgAvatar}
-          source={{
-            uri: "https://ttkddongthap.net/tainguyen/images/thanhtan.jpg",
+        <TextInput
+          style={{
+            width: "100%",
+            height: 50,
+            backgroundColor: "#fff",
+            paddingHorizontal: 50,
+            borderRadius: 10,
+            position: "absolute",
           }}
         />
-        <Text style={styles.txtAvatar}>Huỳnh Bùi Thanh Tân</Text>
+        <Ionicons
+          name="search"
+          size={32}
+          style={{
+            position: "absolute",
+            color: "gray",
+            fontSize: 25,
+            top: 38,
+            left: 15,
+          }}
+        />
       </View>
     </View>
   );
 };
 
 const Content = (props) => {
-  const {data} = props;
+  const { data } = props;
   //console.log(data);
   return (
-
     <View
       style={{
-        flex: 2,
+        flex: 4,
         backgroundColor: App_Color.MauNen_Dark,
         width: "100%",
         alignItems: "center",
       }}
     >
-    <FlatList
-        style={{width:"100%",marginTop:10}}
+      <FlatList
+        style={{ width: "100%", marginTop: 10 }}
         data={data}
-        renderItem={({item})=>{         
-        return(          
-          <TouchableOpacity style={{width: "100%", alignItems: "center"}}>
-          <View
-            style={{
-              flexDirection: "row",
-              width: "90%",
-              paddingHorizontal: 10,
-              paddingVertical: 10,
-              alignItems: "center",
-              marginTop: 10,
-              backgroundColor: "#fff",
-              borderRadius: 10,
-            }}
-          >
-            <Image
-              style={styles.imgUser}
-              source={{
-                uri: item.image,
-              }}
-            />
-            <View>
-              <Text style={styles.txtUser}>{item.tennv}</Text>
-              <Text style={styles.txtChucdanh}>{item.chucdanh}</Text>
-              <Text style={{ color: "gray", fontSize: 10, marginTop: 2 }}>{item.sdt}</Text>
-            </View>
-          </View>
-        </TouchableOpacity>            
-          )
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity style={{ width: "100%", alignItems: "center" }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  width: "90%",
+                  paddingHorizontal: 10,
+                  paddingVertical: 10,
+                  alignItems: "center",
+                  marginTop: 10,
+                  backgroundColor: "#fff",
+                  borderRadius: 10,
+                }}
+              >
+                <Image
+                  style={styles.imgUser}
+                  source={{
+                    uri: item.image,
+                  }}
+                />
+                <View>
+                  <Text style={styles.txtUser}>{item.tennv}</Text>
+                  <Text style={styles.txtChucdanh}>{item.chucdanh}</Text>
+                  <Text style={{ color: "gray", fontSize: 10, marginTop: 2 }}>
+                    {item.sdt}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
         }}
-        keyExtractor={item => item.id}
-        />      
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 };
@@ -151,9 +173,8 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   txtChucdanh: {
-    fontWeight:'600',    
+    fontWeight: "600",
     fontSize: 10,
-    color:"green"
+    color: "green",
   },
-
 });
--- Services/APIs.js
+++ Services/APIs.js
@@ -1,6 +1,6 @@
+const APIs = {
+  getUsersAPI:
+    "https://script.google.com/macros/s/AKfycbwOnPeGXIUEpHiilWekOKvX1DhDGZBiWB5cVzjTSsGj2nDKHI__jBNg7Kdb0V6kTitf/exec?action=getUsers&top=20",
+};
 
-const APIs= {    
-   getUsersAPI :'https://script.google.com/macros/s/AKfycbzAQs_8U46DbZCcL3muHgqwzD9AR55Fgmq5Ea38FFhvQ1z-PwMlbVzS_o9m_rj8R12Y/exec?action=getUsers&top=20&username='
-} 
-
- export default APIs;
+export default APIs;
